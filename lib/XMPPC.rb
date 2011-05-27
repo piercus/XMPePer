a3be5c22b15624b75745502fc44e27fac8a708c4
pierre <piercus@gmail.com>
2011-05-27 16:52:16 +0200

    debug

--- lib/XMPPC.rb
+++ lib/XMPPC.rb
@@ -7,7 +7,7 @@ include Jabber
 class Cl < Client
   attr_reader :messages, :presences, :mc, :name, :muc_messages, :muc_messages, :muc_joins
   USER_CONF = { "sam" => "moi", "kspr" => "moi", "moi" => "moi", "pierre" => "pierre", "pier" => "moi", "toi" => "toi", "pif" => "pif", "paf"=> "paf", "pouf"=>"pouf", "jambon" => "jambon" }
-  ROOM_DEFAULT_NAME = "room"
+  ROOM_DEFAULT_NAME = "room2"
   def initialize(name,host = "localhost", server_name = 'kspr-r720',presence_type = ':available')
 		@name = name
 		@server_name = server_name
@@ -36,7 +36,7 @@ class Cl < Client
 	def connectMUC(room_name = ROOM_DEFAULT_NAME, nick_name = @name+'_in_'+ROOM_DEFAULT_NAME)
 		@mc = Jabber::MUC::SimpleMUCClient.new(self)
 		@mc.on_message do |t,n,m| @muc_messages.push(m) end
-		@mc.on_join do |j,n| @muc_joins.push({:time => t, nick_name => n}) end
+		@mc.on_join do |j,n| @muc_joins.push({:time => t, :nick_name => n}) end
     join_room(room_name,nick_name)
 	end
 	
