fc3d1aea4b23f0338000f455f7695ea8da3d7c05
KspR <s.tonthat@gmail.com>
2011-05-28 16:04:13 +0200

    test.js

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,11 +1,21 @@
 var nickname = "pif", password = "pif",
     servername = "kspr-r720";
-Strophe.log = console.log;
-var c = Srophe.test.connection
-var cb = function(t){
+
+
+
+
+var c = Strophe.Test.connection;
+var cb = function(t,cba){
   return function(){
     console.log(t,arguments);
+		if (cba) cba();
   }
 }
-c.connect("pif@kspr-r720","pif",cb("connect"))
+
+Strophe.log = console.log;
+c.connect("pif@kspr-r720","pif",cb("connect"));
+
+var status = $pres().c('show').t('nil');
+c.send(status); 
+
 c.muc.join("room2@conference.kspr-r720","pif_in_room2",cb("msg"),cb("presence"));
